315b570f5f97872ab6ce4eaebdcd19034e4c9e21
@Sileye <@dev.Sileyesarr@gmail.com>
2026-01-09 12:32:17 -0600

    little changes

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -68,7 +68,7 @@ const AboutSection = () => {
           {/* Content */}
           <div className="space-y-6">
             <h3 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">
-              Get to know me!
+              The Human Behind the Code
             </h3>
             
             <div className="space-y-6 text-gray-600 dark:text-gray-300 leading-relaxed">
